64a4643f17dc725cf7bc544ad4ac626fa18f05a4
AustynYou <austyn.you@gmail.com>
2023-01-24 17:33:30 -0800

    🔧 Add Profile Title

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,22 @@
+import Head from "next/head";
 import AppLayout from '../components/AppLayout';
 
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
-  return <AppLayout>Profile page</AppLayout>
+  return (
+    <>
+     <Head>
+        <title>Profile Page</title>
+    </Head>
+    <AppLayout>
+      <NicknameEditForm />
+      <FollowList header="Following" />
+      <FollowList header="Followers" />
+    </AppLayout>
+    </>
+  )
 }
 
 export default Profile;
