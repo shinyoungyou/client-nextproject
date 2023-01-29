7c2bb5edea3dc11e0dbbf7391ccc32d09d10a726
AustynYou <austyn.you@gmail.com>
2023-01-28 23:47:08 -0800

    🔧 Fix Initial State

--- reducers/user.js
+++ reducers/user.js
@@ -1,6 +1,44 @@
 export const initialState = {
   isLoggedIn: false,
-  me: null
+  my: null,
+  User: [
+    {
+      id: 1,
+      username: "Shrimp and Chorizo Paella",
+      email: "shrimp@gmail.com",
+      pass: "test1",
+      Followings: null,
+      Followers: null,
+      Posts: null
+    },
+    {
+      id: 2,
+      username: "Mary S Bing",
+      email: "msbing@gmail.com",
+      pass: "test2",
+      Followings: null,
+      Followers: null,
+      Posts: null
+    },
+    {
+      id: 3,
+      username: "Rose A Kramer",
+      email: "rose@gmail.com",
+      pass: "test3",
+      Followings: null,
+      Followers: null,
+      Posts: null
+    },
+    {
+      id: 4,
+      username: "Joy T Jones",
+      email: "joy@gmail.com",
+      pass: "test4",
+      Followings: null,
+      Followers: null,
+      Posts: null
+    },
+  ]
 }
 
 export const loginAction = (data) => {
@@ -14,19 +52,84 @@ export const logoutAction = {
   type: 'LOG_OUT',
 }
 
+export const dummyMy =  {
+  id: 1,
+  username: "Shrimp and Chorizo Paella",
+  email: "shrimp@gmail.com",
+  pass: "test1",
+  Followings: null,
+  Followers: null,
+  Posts: [{
+    id: 1,
+    content: "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+    createdAt: "September 14, 2016",
+    User: {
+      id: 1,
+      username: "Shrimp and Chorizo Paella"
+    },
+    Likers: [
+      {
+        id: 2
+      },
+      {
+        id: 3
+      },
+      {
+        id: 4
+      },
+    ],
+    RetweetId: null,
+    Retweet: null,
+    Images: [{
+      src: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Sashimi_-_Maguro_Restaurant%2C_Bangna%2C_Bangkok_%2844856596864%29.jpg",
+      alt: "Salmon_Sushi.jpg"
+    }],
+    Comments: [
+      {
+        id: 1,
+        content: "Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.",
+        createdAt: "",
+        User: {
+          id: 2,
+          username: "Mary S Bing"
+        },
+      },
+      {
+        id: 2,
+        content: "Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo.",
+        createdAt: "",
+        User: {
+          id: 3,
+          username: "Rose A Kramer"
+
+        },
+      },
+      {
+        id: 3,
+        content: "Add rice and stir very gently to distribute. Top with artichokes andpeppers, and cook without stirring, until most of the liquid is absorbed,15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp andmussels, tucking them down into the rice, and cook again.",
+        createdAt: "",
+        User: {
+          id: 4,
+          username: "Joy T Jones"
+
+        },
+      },
+    ]
+  }]
+}
 const reducer = (state = initialState, action) => {
   switch (action.type){
     case 'LOG_IN':
       return {
           ...state,
           isLoggedIn: true,
-          me: action.data
+          my: action.data
         }
     case 'LOG_OUT':
       return {
           ...state,
           isLoggedIn: false,
-          me: null
+          my: null
         }
     default:
       return state;
