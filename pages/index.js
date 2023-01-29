349734a985c346d8874c6f1541e27ed03cea81bf
AustynYou <austyn.you@gmail.com>
2023-01-28 23:46:17 -0800

    👀 Display Main Posts from Global State

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-import AppLayout from '../components/AppLayout';
+import { useSelector } from 'react-redux';
 
+import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
 const Home = () => {
-  return (<AppLayout>Hello, Next!</AppLayout>)
+  const { isLoggedIn } = useSelector((state)=>state.user);
+  const mainPosts = useSelector((state)=>state.post.mainPosts);
+
+  return (
+    <>
+      <AppLayout>
+        {isLoggedIn && <PostForm/>}
+        {mainPosts.map((post)=>(<PostCard post={post} key={post.id} />))}
+      </AppLayout>
+    </>
+  )
 }
 
 export default Home;
