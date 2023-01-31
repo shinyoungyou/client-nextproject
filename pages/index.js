5241885a634a274593cb2da1d638a4ceae1c3fae
AustynYou <austyn.you@gmail.com>
2023-01-30 17:06:50 -0800

    🚧 LOG_IN, LOG_OUT works

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 const Home = () => {
-  const { isLoggedIn } = useSelector((state)=>state.user);
+  const { my } = useSelector((state)=>state.user);
   const mainPosts = useSelector((state)=>state.post.mainPosts);
 
   return (
     <>
       <AppLayout>
-        {isLoggedIn && <PostForm/>}
+        {my && <PostForm/>}
         {mainPosts.map((post)=>(<PostCard post={post} key={post.id} />))}
       </AppLayout>
     </>
