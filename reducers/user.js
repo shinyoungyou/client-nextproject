8b67dd0f922a71f4d98898e31e1df02d24a07fdd
AustynYou <austyn.you@gmail.com>
2023-01-31 15:53:05 -0800

    🚧 Use immer library to Reducer

--- reducers/user.js
+++ reducers/user.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 export const initialState = {
   logInLoading: false,
   logInDone: false,
@@ -5,6 +7,18 @@ export const initialState = {
   logOutLoading: false,
   logOutDone: false,
   logOutError: null,
+  signUpLoading: false,
+  signUpDone: false,
+  signUpError: null,
+  followLoading: false,
+  followDone: false,
+  followError: null,
+  unfollowLoading: false,
+  unfollowDone: false,
+  unfollowError: null,
+  changeNicknameLoading: false,
+  changeNicknameDone: false,
+  changeNicknameError: null,
   my: null,
   User: [
     {
@@ -54,6 +68,22 @@ export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
 export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
 export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
 
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
+export const CHANGE_NICKNAME_REQUEST = 'CHANGE_NICKNAME_REQUEST';
+export const CHANGE_NICKNAME_SUCCESS = 'CHANGE_NICKNAME_SUCCESS';
+export const CHANGE_NICKNAME_FAILURE = 'CHANGE_NICKNAME_FAILURE';
+
 export const loginRequestAction = (data) => {
   return {
     type: LOG_IN_REQUEST,
@@ -65,54 +95,113 @@ export const logoutRequestAction = {
   type: LOG_OUT_REQUEST,
 }
 
-const reducer = (state = initialState, action) => {
+export const signupRequestAction = (data) => {
+  return {
+    type: SIGN_UP_REQUEST,
+    data
+  }
+}
+
+export const followRequestAction = (data) => {
+  return {
+    type: FOLLOW_REQUEST,
+    data
+  }
+}
+
+export const unfollowRequestAction = (data) => {
+  return {
+    type: UNFOLLOW_REQUEST,
+    data
+  }
+}
+
+const reducer = (state = initialState, action) => produce(state, (draft)=>{
   switch (action.type){
     case LOG_IN_REQUEST:
-      console.log('reducer login');
-      return {
-        ...state,
-        logInLoading: true,
-        logInDone: false,
-        logInError: null,
-      }
+      draft.logInLoading = true;
+      draft.logInDone = false;
+      draft.logInError = null;
+      break;
     case LOG_IN_SUCCESS:
-      return {
-        ...state,
-        logInLoading: false,
-        my: action.data,
-        logInDone: true,
-      }
+      draft.logInLoading = false;
+      draft.my = action.data;
+      draft.logInDone = true;
+      break;
     case LOG_IN_FAILURE:
-      return {
-        ...state,
-        logInLoading: false,
-        logInError: action.error,
-      }
+      draft.logInLoading = false;
+      draft.logInError = action.error;
+      break;
     case LOG_OUT_REQUEST:
-      return {
-        ...state,
-        logOutLoading: true,
-        logOutDone: false,
-        logOutError: null,
-      }
+      draft.logOutLoading = true;
+      draft.logOutDone = false;
+      draft.logOutError = null;
+      break;
     case LOG_OUT_SUCCESS:
-      console.log('reducer logout');
-      return {
-        ...state,
-        logOutLoading: false,
-        logOutDone: true,
-        my: null
-      }
+      draft.logOutLoading = false;
+      draft.my = null;
+      draft.logOutDone = true;
+      break;
     case LOG_OUT_FAILURE:
-      return {
-        ...state,
-        logOutLoading: false,
-        logOutError: action.error,
-      }
+      draft.logOutLoading = false;
+      draft.logOutError = action.error;
+      break;
+    case SIGN_UP_REQUEST:
+      draft.signUpLoading = true;
+      draft.signUpDone = false;
+      draft.signUpError = null;
+      break;
+    case SIGN_UP_SUCCESS:
+      draft.signUpLoading = false;
+      draft.signUpDone = true;
+      break;
+    case SIGN_UP_FAILURE:
+      draft.signUpLoading = false;
+      draft.signUpError = action.error;
+      break;
+    case FOLLOW_REQUEST:
+      draft.followLoading = true;
+      draft.followDone = false;
+      draft.followError = null;
+      break;
+    case FOLLOW_SUCCESS:
+      draft.followLoading = false;
+      draft.followDone = true;
+      break;
+    case FOLLOW_FAILURE:
+      draft.followLoading = false;
+      draft.followError = action.error;
+      break;
+    case UNFOLLOW_REQUEST:
+      draft.unfollowLoading = true;
+      draft.unfollowDone = false;
+      draft.unfollowError = null;
+      break;
+    case UNFOLLOW_SUCCESS:
+      draft.unfollowLoading = false;
+      draft.unfollowDone = true;
+      break;
+    case UNFOLLOW_FAILURE:
+      draft.unfollowLoading = false;
+      draft.unfollowError = action.error;
+      break;
+    case CHANGE_NICKNAME_REQUEST:
+      draft.changeNicknameLoading = true;
+      draft.changeNicknameDone = false;
+      draft.changeNicknameError = null;
+      break;
+    case CHANGE_NICKNAME_SUCCESS:
+      draft.changeNicknameLoading = false;
+      draft.changeNicknameDone = true;
+      break;
+    case CHANGE_NICKNAME_FAILURE:
+      draft.changeNicknameLoading = false;
+      draft.changeNicknameError = action.error;
+      break;
     default:
       return state;
   }
-}
+})
 
 export const dummyMy =  {
   id: 1,
