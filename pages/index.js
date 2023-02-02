241769a71b16fda90c634fc4c78dcdf41d7bfbb7
AustynYou <austyn.you@gmail.com>
2023-02-02 13:58:43 -0800

    🚧 Implement Infinite Scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadPostsRequest } from "../reducers/post";
+import { useEffect } from "react";
 
 import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 const Home = () => {
   const { my } = useSelector((state)=>state.user);
-  const mainPosts = useSelector((state)=>state.post.mainPosts);
+  const { mainPosts, loadPostsLoading, bringMorePosts } = useSelector((state)=>state.post);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(loadPostsRequest());
+  }, [])
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 700){
+        if(!loadPostsLoading && bringMorePosts){
+          dispatch(loadPostsRequest());
+        }
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [loadPostsLoading, bringMorePosts])
 
   return (
     <>
