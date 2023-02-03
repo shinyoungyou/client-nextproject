04ee529af475dcd34c8cf530ce426b7be4c60d48
AustynYou <austyn.you@gmail.com>
2023-02-02 17:28:54 -0800

    🚧 Implement global profile navigators

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { logoutRequestAction } from '../reducers/user';
+import { logoutRequestAction, navigateProfileAction } from '../reducers/user';
+
+import { useState } from 'react';
+import { useRouter } from 'next/router'
 
 import { Avatar, Box, Button, BottomNavigation, BottomNavigationAction, Card, CardHeader, CardContent, CardActions } from '@mui/material';
 import { blue } from '@mui/material/colors';
@@ -12,21 +13,26 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { LoadingButton } from '@mui/lab';
 
 const UserProfile = () => {
-  const { my, logOutLoading } = useSelector((state)=>state.user);
+  const { my, logOutLoading, navigateProfile } = useSelector((state)=>state.user);
   const dispatch = useDispatch();
+  const router = useRouter();
 
-  const [value, setValue] = useState(0);
-  const handleLogout = () =>{
+  const handleLogout = () => {
     dispatch(logoutRequestAction);
+  }
+
+  const RedirectToHome = () => {
+    router.push('/')
+  }
+  const RedirectToProfile = () => {
+    router.push('/profile')
   }
 
   return (
     <Card sx={{ maxWidth: 345, margin: '8px 0' }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: blue[500] }}aria-label="recipe">
-            S
-          </Avatar>
+          <Avatar alt={my.username} src="/static/images/avatar/1.jpg" sx={{ bgcolor: blue[500] }} aria-label="recipe" />
         }
         action={
           <LoadingButton onClick={handleLogout} variant="outlined" loading={logOutLoading} style={{ margin: '4px 8px' }}>Log Out</LoadingButton>
@@ -37,15 +43,15 @@ const UserProfile = () => {
       <Box sx={{ width: 500 }}>
       <BottomNavigation
         showLabels
-        value={value}
+        value={navigateProfile}
         onChange={(event, newValue) => {
-          setValue(newValue);
+          dispatch(navigateProfileAction(newValue));
         }}
       >
 
-        <BottomNavigationAction label="Tweets" icon={my.Posts ? my.Posts.length : 0} />
-        <BottomNavigationAction label="Following" icon={my.Followings ? my.Followings.length: 0}  />
-        <BottomNavigationAction label="Followers" icon={my.Followers ? my.Followers.length: 0} />
+        <BottomNavigationAction onClick={RedirectToHome} label="Tweets" icon={my.Posts ? my.Posts.length : 0} />
+        <BottomNavigationAction onClick={RedirectToProfile} label="Following" icon={my.Followings ? my.Followings.length: 0} />
+        <BottomNavigationAction onClick={RedirectToProfile} label="Followers" icon={my.Followers ? my.Followers.length: 0} />
 
       </BottomNavigation>
       </Box>
