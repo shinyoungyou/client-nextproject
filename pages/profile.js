a1e1891bc64258ee6da7391e3e80c714668624c6
AustynYou <austyn.you@gmail.com>
2023-02-02 22:35:31 -0800

    🚧 Let only Logged User can go Profile Page

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,20 @@
+import { useSelector } from "react-redux";
 import Head from "next/head";
-import AppLayout from '../components/AppLayout';
+import { useRouter } from 'next/router'
+import { useEffect } from "react";
 
+import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  const { my } = useSelector((state)=>state.user);
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(!my) router.push('/')
+  }, [my])
+
   return (
     <>
      <Head>
@@ -12,8 +22,8 @@ const Profile = () => {
     </Head>
     <AppLayout>
       <NicknameEditForm />
-      <FollowList header="Following" />
-      <FollowList header="Followers" />
+      <FollowList header="Followings" item={my?.Followings} />
+      <FollowList header="Followers" item={my?.Followers}/>
     </AppLayout>
     </>
   )
