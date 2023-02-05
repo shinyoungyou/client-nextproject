3df99a65c009d657572fe3421092376ffabc2138
AustynYou <austyn.you@gmail.com>
2023-02-05 04:45:08 -0800

    ✍️ Add Typescript to User Action Creators

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -3,6 +3,18 @@ import produce from 'immer';
 import shortId from 'shortid';
 import { faker } from '@faker-js/faker/locale/en_CA';
 
+import {
+  LogInActionType,
+  LogOutActionType,
+  SignUpActionType,
+  FollowActionType,
+  UnfollowActionType,
+  ChangeNicknameActionType,
+  AddPostToMeActionType,
+  RemovePostOfMeActionType,
+  NavigateProfileActionType,
+} from '../action-types/user';
+
 export const initialState = {
   logInLoading: false,
   logInDone: false,
@@ -64,165 +76,97 @@ export const initialState = {
   ]
 }
 
-export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
-export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
-
-export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
-export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
-export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
-
-export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
-export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
-export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
-
-export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
-export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
-export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
-
-export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
-export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
-export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
-
-export const CHANGE_NICKNAME_REQUEST = 'CHANGE_NICKNAME_REQUEST';
-export const CHANGE_NICKNAME_SUCCESS = 'CHANGE_NICKNAME_SUCCESS';
-export const CHANGE_NICKNAME_FAILURE = 'CHANGE_NICKNAME_FAILURE';
-
-export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
-export const REMOVE_POST_OF_ME = 'REMOVE_POST_OF_ME';
-
-export const NAVIGATE_PROFILE = 'NAVIGATE_PROFILE';
-
-export const loginRequestAction = (data) => {
-  return {
-    type: LOG_IN_REQUEST,
-    data
-  }
-}
-
-export const logoutRequestAction = {
-  type: LOG_OUT_REQUEST,
-}
-
-export const signupRequestAction = (data) => {
-  return {
-    type: SIGN_UP_REQUEST,
-    data
-  }
-}
-
-export const followRequestAction = (data) => {
-  return {
-    type: FOLLOW_REQUEST,
-    data
-  }
-}
-
-export const unfollowRequestAction = (data) => {
-  return {
-    type: UNFOLLOW_REQUEST,
-    data
-  }
-}
-
-export const navigateProfileAction = (data) => {
-  return {
-    type: NAVIGATE_PROFILE,
-    data
-  }
-}
-
 const reducer = (state = initialState, action) => produce(state, (draft)=>{
   switch (action.type){
-    case LOG_IN_REQUEST:
+    case LogInActionType.LOG_IN_REQUEST:
       draft.logInLoading = true;
       draft.logInDone = false;
       draft.logInError = null;
       break;
-    case LOG_IN_SUCCESS:
+    case LogInActionType.LOG_IN_SUCCESS:
       draft.logInLoading = false;
       draft.my = action.data;
       draft.logInDone = true;
       break;
-    case LOG_IN_FAILURE:
+    case LogInActionType.LOG_IN_FAILURE:
       draft.logInLoading = false;
       draft.logInError = action.error;
       break;
-    case LOG_OUT_REQUEST:
+    case LogOutActionType.LOG_OUT_REQUEST:
       draft.logOutLoading = true;
       draft.logOutDone = false;
       draft.logOutError = null;
       break;
-    case LOG_OUT_SUCCESS:
+    case LogOutActionType.LOG_OUT_SUCCESS:
       draft.my = null;
       draft.logOutLoading = false;
       draft.logOutDone = true;
       break;
-    case LOG_OUT_FAILURE:
+    case LogOutActionType.LOG_OUT_FAILURE:
       draft.logOutLoading = false;
       draft.logOutError = action.error;
       break;
-    case SIGN_UP_REQUEST:
+    case SignUpActionType.SIGN_UP_REQUEST:
       draft.signUpLoading = true;
       draft.signUpDone = false;
       draft.signUpError = null;
       break;
-    case SIGN_UP_SUCCESS:
+    case SignUpActionType.SIGN_UP_SUCCESS:
       draft.signUpLoading = false;
       draft.signUpDone = true;
       break;
-    case SIGN_UP_FAILURE:
+    case SignUpActionType.SIGN_UP_FAILURE:
       draft.signUpLoading = false;
       draft.signUpError = action.error;
       break;
-    case FOLLOW_REQUEST:
+    case FollowActionType.FOLLOW_REQUEST:
       draft.followLoading = true;
       draft.followDone = false;
       draft.followError = null;
       break;
-    case FOLLOW_SUCCESS:
+    case FollowActionType.FOLLOW_SUCCESS:
       draft.my.Followings.unshift({ id: action.data.id, username: action.data.username });
       draft.followLoading = false;
       draft.followDone = true;
       break;
-    case FOLLOW_FAILURE:
+    case FollowActionType.FOLLOW_FAILURE:
       draft.followLoading = false;
       draft.followError = action.error;
       break;
-    case UNFOLLOW_REQUEST:
+    case UnfollowActionType.UNFOLLOW_REQUEST:
       draft.unfollowLoading = true;
       draft.unfollowDone = false;
       draft.unfollowError = null;
       break;
-    case UNFOLLOW_SUCCESS:
+    case UnfollowActionType.UNFOLLOW_SUCCESS:
       draft.my.Followings = draft.my.Followings.filter((user) => user.id !== action.data);
       draft.unfollowLoading = false;
       draft.unfollowDone = true;
       break;
-    case UNFOLLOW_FAILURE:
+    case UnfollowActionType.UNFOLLOW_FAILURE:
       draft.unfollowLoading = false;
       draft.unfollowError = action.error;
       break;
-    case CHANGE_NICKNAME_REQUEST:
+    case ChangeNicknameActionType.CHANGE_NICKNAME_REQUEST:
       draft.changeNicknameLoading = true;
       draft.changeNicknameDone = false;
       draft.changeNicknameError = null;
       break;
-    case CHANGE_NICKNAME_SUCCESS:
+    case ChangeNicknameActionType.CHANGE_NICKNAME_SUCCESS:
       draft.changeNicknameLoading = false;
       draft.changeNicknameDone = true;
       break;
-    case CHANGE_NICKNAME_FAILURE:
+    case ChangeNicknameActionType.CHANGE_NICKNAME_FAILURE:
       draft.changeNicknameLoading = false;
       draft.changeNicknameError = action.error;
       break;
-    case ADD_POST_TO_ME:
+    case AddPostToMeActionType.ADD_POST_TO_ME:
       draft.my.Posts.unshift({ id: action.data });
       break;
-    case REMOVE_POST_OF_ME:
+    case RemovePostOfMeActionType.REMOVE_POST_OF_ME:
       draft.my.Posts = draft.my.Posts.filter((post) => post.id !== action.data);
       break;
-    case NAVIGATE_PROFILE:
+    case NavigateProfileActionType.NAVIGATE_PROFILE:
       draft.navigateProfile = action.data
       break;
     default:
