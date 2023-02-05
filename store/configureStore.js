9b2c618adf1f723df0634b06ccbe77e4a98ba323
AustynYou <austyn.you@gmail.com>
2023-02-05 04:50:37 -0800

    🚚 Update folder structure

--- store/configureStore.js
+++ store/configureStore.js
@@ -3,8 +3,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from 'redux-saga';
 
-import reducer from '../reducers';
-import rootSaga from '../sagas';
+import reducer from './reducers';
+import rootSaga from './sagas';
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
