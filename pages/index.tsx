5435a3dce0ce4164a5b84696b805c938debc02ca
AustynYou <austyn.you@gmail.com>
2023-02-05 19:33:14 -0800

    ✍️ Typescripted Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { loadPostsRequest } from "../store/action-creators/post";
 import { useEffect } from "react";
+import type { NextPage } from 'next';
+import RootState from "../store/state-types";
 
 import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
-const Home = () => {
-  const { my } = useSelector((state)=>state.user);
-  const { mainPosts, loadPostsLoading, bringMorePosts } = useSelector((state)=>state.post);
+const Home: NextPage = () => {
+  const { my } = useSelector((state: RootState)=>state.user);
+  const { mainPosts, loadPostsLoading, bringMorePosts } = useSelector((state: RootState)=>state.post);
   const dispatch = useDispatch();
 
   useEffect(()=>{
