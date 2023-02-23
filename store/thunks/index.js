fed691f32e4d3320c46f97fe0fc8733d7b38c11d
AustynYou <austyn.you@gmail.com>
2023-02-23 15:16:28 -0800

    🔧 Set up axios instance baseURL

---
+++ store/thunks/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `http://localhost:3065`,
+});
+// instance.interceptors.request.use(function (instance) {
+//   const kakao_authorization = localStorage.getItem("kakao_authorization");
+//   const Token = localStorage.getItem("Authorization");
+//   const Retoken = localStorage.getItem("refresh-Token");
+//   instance.headers["Authorization"] = Token;
+//   instance.headers["refresh-Token"] = Retoken;
+//   instance.headers["kakao_authorization"] = kakao_authorization;
+//   return instance;
+// });
+
+export default instance;
