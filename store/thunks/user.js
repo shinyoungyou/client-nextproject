37ece9d83888ceae491fead1814861ca4177f998
AustynYou <austyn.you@gmail.com>
2023-03-26 13:07:52 -0700

    🚧 Implement  thunk APIs: loadFollowings, follow, unfollow, loadFollowers, removeFollower

--- store/thunks/user.js
+++ store/thunks/user.js
@@ -43,26 +43,6 @@ export const signUp = createAsyncThunk('user/signUp', async (payload, thunkAPI) 
     }
 })
 
-export const follow = createAsyncThunk('user/follow', async (payload, thunkAPI) => {
-    try {
-        // const response = await instance.post('user/follow', payload);
-        await pause(1000);
-        return thunkAPI.fulfillWithValue(payload); // pass to extraReducer
-    } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
-    }
-})
-
-export const unfollow = createAsyncThunk('user/unfollow', async (payload, thunkAPI) => {
-    try {
-        // const response = await instance.delete(`user/follow/${payload.id}`);
-        await pause(1000);
-        return thunkAPI.fulfillWithValue(payload); // pass to extraReducer
-    } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
-    }
-})
-
 export const changeUsername = createAsyncThunk('user/changeUsername', async (payload, thunkAPI) => {
     try {
         const response = await instance.patch('user/profile', payload);
@@ -72,3 +52,53 @@ export const changeUsername = createAsyncThunk('user/changeUsername', async (pay
         return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
     }
 })
+
+export const loadFollowings = createAsyncThunk('user/loadFollowings', async (payload, thunkAPI) => {
+    try {
+        const response = await instance.get('user/followings');
+        return thunkAPI.fulfillWithValue(response.data); // pass to extraReducer
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
+    }
+})
+
+export const follow = createAsyncThunk('user/follow', async (payload, thunkAPI) => {
+    try {
+        const response = await instance.put(`user/following/${payload.id}`, payload);
+        // await pause(1000);
+        return thunkAPI.fulfillWithValue(response.data); // pass to extraReducer
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
+    }
+})
+
+export const unfollow = createAsyncThunk('user/unfollow', async (payload, thunkAPI) => {
+    try {
+        const response = await instance.delete(`user/following/${payload.id}`);
+        // await pause(1000);
+        return thunkAPI.fulfillWithValue(response.data); // pass to extraReducer
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
+    }
+})
+
+export const loadFollowers = createAsyncThunk('user/loadFollowers', async (payload, thunkAPI) => {
+    try {
+        const response = await instance.get('user/followers');
+        return thunkAPI.fulfillWithValue(response.data); // pass to extraReducer
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
+    }
+})
+
+export const removeFollower = createAsyncThunk('user/removeFollower', async (payload, thunkAPI) => {
+    try {
+        const response = await instance.delete(`user/follower/${payload.id}`);
+        // await pause(1000);
+        return thunkAPI.fulfillWithValue(response.data); // pass to extraReducer
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data); // pass to extraReducer
+    }
+})
+
+
