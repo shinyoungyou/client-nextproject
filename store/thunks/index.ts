851928e01d866fa29ba007cdff70f2217ce71a06
AustynYou <austyn.you@gmail.com>
2023-05-10 19:19:48 -0700

    🔧 cookie not stored issue in front

--- store/thunks/index.ts
+++ store/thunks/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { backUrl } from "../../config/config";
 
+axios.defaults.baseURL = backUrl;
+axios.defaults.withCredentials = true;
+
 const instance = axios.create({
   baseURL: backUrl, // cors
   withCredentials: true, // cookie (user info)
